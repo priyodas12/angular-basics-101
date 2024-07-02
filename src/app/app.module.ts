3afd1429e390dad35f62101056051c26a61c9565
priyonrato.das <priyodas12@gmail.com>
2024-07-02 23:34:52 +0530

    added new component and angular metrial

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,17 +3,26 @@ import { BrowserModule, provideClientHydration } from '@angular/platform-browser
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { UsersComponent } from './users/users.component';
+import { provideAnimationsAsync } from '@angular/platform-browser/animations/async';
+
+import { MatCardModule }  
+    from '@angular/material/card'; 
+
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    UsersComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    MatCardModule
   ],
   providers: [
-    provideClientHydration()
+    provideClientHydration(),
+    provideAnimationsAsync()
   ],
   bootstrap: [AppComponent]
 })
